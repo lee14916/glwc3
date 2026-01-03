b3ece038f02bddf1604886dc6e0478167c9f40cf
Yan Li <yan1491625@gmail.com>
2026-01-03 09:39:30 +0200

    sync

--- project2/05_moments/dataPrepare/scp.sh
+++ project2/05_moments/dataPrepare/scp.sh
@@ -1,4 +1,5 @@
 # hostname=li47@login.jupiter.fz-juelich.de
 hostname=li47@juwels-booster.fz-juelich.de
 
+# rsync -av --progress /nvme/h/cy22yl1/projectData/05_moments/cB211.072.64/data_merge/ ${hostname}:/p/project1/ngff/li47/code/projectData/05_moments/cyclone/
 rsync -av --progress /nvme/h/cy22yl1/projectData/05_moments/ ${hostname}:/p/project1/ngff/li47/code/projectData/05_moments/cyclone/
